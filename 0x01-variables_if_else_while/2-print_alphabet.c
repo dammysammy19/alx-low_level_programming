024d53c8fc3828066bbae1036805291ffc32d1f7
Damilola Assan <dammysammy19@gmail.com>
2022-03-14 07:41:44 -0700

    new try

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
  * main - prints the alphabet in lowercase, followed by a new line
  * Return: Always 0 (Success)
  */
@@ -12,6 +12,5 @@ int main(void)
 	{
 		putchar(ch);
 	}
-	putchar('\n');
 	return (0);
 }
