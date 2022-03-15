e6d49badb1ba22b1aec868bfd2d7d5198a868ca9
Damilola Assan <dammysammy19@gmail.com>
2022-03-15 09:04:00 -0700

    added Readme and the putchar task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char* text = "_putchar";
+	int i = 0;
+
+	while (text[i])
+	{
+		_putchar(text[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
