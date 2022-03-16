097be6b9f18b116403d391f3b91e30ab4611922c
Damilola Assan <dammysammy19@gmail.com>
2022-03-15 21:02:24 -0700

    2nd try

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - print alphabet
  *
  * Return: Always 0 (Success)
  */
- 
-void print_alphabet(void) 
+
+void print_alphabet(void)
 {
 	char c = 'a';
-        while   (c <=  'z')
+
+	while (c <=  'z')
 	{
 		_putchar(c);
 		c++;
