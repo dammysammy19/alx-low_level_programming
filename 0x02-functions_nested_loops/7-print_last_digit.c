aaa430efba1480ae54b70023720f37b2478a860d
Damilola Assan <dammysammy19@gmail.com>
2022-03-16 07:25:14 -0700

    print last digit 2

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,16 +1,14 @@
 #include "main.h"
 
 /**
- * print_last-digit - check the code for Holberton School students.
+ * print_last_digit - check the code for Holberton School students.
+ * @r: r is an integer
  * Return: Always 0.
  */
 int print_last_digit(int r)
 {
+	int n = r % 10;
 
-	print_last_digit(98);
-	print_last_digit(0);
-	r = print_last_digit(-1024);
-	_putchar('0' + r);
-	_putchar('\n');
-	return (0);
+	_putchar(n + 48);
+	return (n);
 }
