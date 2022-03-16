dd4cf03fde7671c538dcd354614786b630c3caf8
Damilola Assan <dammysammy19@gmail.com>
2022-03-16 06:41:38 -0700

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last-digit - check the code for Holberton School students.
+ * Return: Always 0.
+ */
+int print_last_digit(int r)
+{
+
+	print_last_digit(98);
+	print_last_digit(0);
+	r = print_last_digit(-1024);
+	_putchar('0' + r);
+	_putchar('\n');
+	return (0);
+}
